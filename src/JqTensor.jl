59c711b52b4bcb5c204b6f3510a74e85dc60acdb
Toshio Mori <forest1040@gmail.com>
2022-09-08 15:37:03 +0900

    first commit

--- src/JqTensor.jl
+++ src/JqTensor.jl
@@ -1,5 +1,69 @@
 module JqTensor
 
-# Write your package code here.
+using ITensors, LinearAlgebra, Random
+
+import Base: length,
+			 copy,
+			 push!,
+			 isless,
+			 delete!,
+			 getindex,
+			 setindex,
+			 findfirst,
+			 deleteat!,
+			 print,
+			 show,
+			 replace!,
+			 reverse,
+			 size,
+			 +,
+			 -,
+			 *,
+			 /,
+			 ==,
+			 isapprox,
+			 pop!,
+			 range,
+			 clamp
+import LinearAlgebra: norm,
+					  normalize!
+import ITensors:getindex,
+				 noprime,
+				 noprime!,
+				 prime,
+				 svd,
+				 qr,
+				 position!,
+				 setindex!,
+				 commonindex,
+				 iterate,
+				 ITensor,
+				 Index,
+				 norm,
+				 order,
+				 *,
+				 dag
+
+# functions from Base, ITensors
+export linkindex,
+	   getindex,
+	   noprime,
+	   noprime!,
+	   prime,
+	   svd,
+	   position!,
+	   setindex!,
+	   svd,
+	   commonindex,
+	   leftLim,
+	   rightLim,
+	   iterate,
+	   ITensor,
+	   Index,
+	   randomITensor,
+	   qr,
+	   IndexSet,
+	   order,
+	   norm
 
 end
